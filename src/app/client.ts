9cc48dd6bf41007231bea73ac927f32d41dc7f9c
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-05-28 20:06:18 -0300

    ForcedoresDarkSouls

---
+++ src/app/client.ts
@@ -0,0 +1,9 @@
+export interface Client{
+    id : number;
+    name : string;
+    email: string;
+    location: string;
+    cpf: number;
+    keys: string; //voce possui a chave certa?
+    class: string;
+  }
---
+++ src/app/fornecedor.ts
@@ -0,0 +1,9 @@
+export interface supplier{
+    id: number;
+    name: string;
+    email: string;
+    location: string;
+    cpf: number;
+    nivel: string;//Escrever em que parte do jogo esse boss aparece
+    dificulcudade: string;
+}
